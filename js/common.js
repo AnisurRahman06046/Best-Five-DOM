2acf3e2745ed5319b5bfeefd6806a551bfc8cb5c
AnisurRahman06046 <anisurrahman06046@gmail.com>
2022-08-23 07:24:22 +0600

    calculation and js file,common js file added

--- js/common.js
+++ js/common.js
@@ -1,1 +1,18 @@
-console.log('js added')
+function getInputElementValueById(elementId){
+    const textElementField = document.getElementById(elementId);
+    const textElementString = textElementField.value ;
+    const textElement = parseInt(textElementString);
+    return textElement;
+}
+
+function getTextElementValueById(textElementId){
+    const textElementField = document.getElementById(textElementId);
+    const textElementString = textElementField.innerText;
+    const textElement = parseInt(textElementString);
+    textElementField.innerText = textElement;
+}
+
+function setTextElementValue(elementID,newValue){
+    const textElement = document.getElementById(elementID);
+    textElement.innerText = newValue;
+}
--- js/functionalities.js
+++ js/functionalities.js
@@ -16,12 +16,33 @@ for (let button of buttons){
             alert('can not add more than five');
             return;
         }
-        
+
         listContainer.appendChild(li);
-        
-       
-        
-        
+         
     })
 }
+
+
+document.getElementById('button-calculate').addEventListener('click',function(){
+    const playerCost = getInputElementValueById('player-cost');
+    const allPlayerExpense = playerCost*(arr.length);
+    
+    const playerExpenses = setTextElementValue('player-expense',allPlayerExpense);
+ 
+})
+
+document.getElementById('total-calculate').addEventListener('click',function(){
+    const managerField = getInputElementValueById('manager-field');
+    const coachField = getInputElementValueById('coach-field');
+    const allPlayerExpensetext = document.getElementById('player-expense');
+    const allPlayerExpenseString = allPlayerExpensetext.innerText;
+    const allPlayerExpense = parseInt(allPlayerExpenseString);
+    
+
+    // total expenses 
+    const totalCalculation = managerField + coachField + allPlayerExpense;
+    const total = setTextElementValue('total-expense',totalCalculation);
+
+
+})
 
