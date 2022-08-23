1d57b0ab7ff3b999983a7887be9d9e335cfd9294
AnisurRahman06046 <anisurrahman06046@gmail.com>
2022-08-23 07:59:24 +0600

    bonus part:btn disabled added

--- js/functionalities.js
+++ js/functionalities.js
@@ -1,31 +1,31 @@
 const buttons = document.getElementsByClassName('playerBtn');
-const arr = []
+let arr = [];
 for (let button of buttons){
     button.addEventListener('click',function(event){
+        event.target.disabled = true;
         const element = event.target.parentNode.children[0].innerText;
-        
         const playername = {
             element:element,
-        }
+        };
         
         const listContainer = document.getElementById('ordered-list');
         const li = document.createElement('li');
         li.innerText = element;
-        arr.push(playername)
+        arr.push(playername);
         if(arr.length>5){
-            alert('can not add more than five');
+            alert('can not add more than 5');
             return;
         }
 
         listContainer.appendChild(li);
-         
+    
     })
 }
 
 
 document.getElementById('button-calculate').addEventListener('click',function(){
     const playerCost = getInputElementValueById('player-cost');
-    const allPlayerExpense = playerCost*(arr.length);
+    const allPlayerExpense = playerCost*5;
     
     const playerExpenses = setTextElementValue('player-expense',allPlayerExpense);
  
